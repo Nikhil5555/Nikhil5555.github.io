d5f22d215bc9f919dc17195acdfe467cabcfec08
Nikhil5555 <52348118+Nikhil5555@users.noreply.github.com>
2019-11-05 05:53:42 -0800

    my second commit

--- portfolio/feedback.php
+++ portfolio/feedback.php
@@ -2,13 +2,27 @@
 $x=$_POST['username'];
 $y=$_POST['email'];
 $z=$_POST['msg'];
-$to="nikhi.18beceg012@gmail.com";
 $subject="Response from website";
-$header="From: $y";
-if(mail($to,$subject,$z,$header)){
-echo "Sent Successfully! Thank you $x for contacting us, we will reply you shortly.";
+require 'PHPMailer-master/PHPMailerAutoload.php';
+$mail=new PHPMailer();
+$mail ->IsSmtp();
+$mail ->SMTPDebug=1;
+$mail ->SMTPAuth=true;
+$mail ->SMTPSecure='ssl';
+$mail ->Host="smpt.gmail.com";
+$mail ->Port=465;
+$mail ->IsHTML(TRUE);
+$mail ->Username="nikhil.18beceg012@gmail.com";
+$mail ->Password="@notankisala5";
+$mail ->Setfrom("nikhil.18beceg012@gmail.com");
+$mail ->Subject =$subject;
+$mail ->Body=$msg;
+$mail ->AddAddress("nikhil.18beceg012@gmail.com");
+if(!$mail ->send())
+{
+  echo "mail not sent";
 }
 else{
-    echo "failed to send";
+    echo "mail sent successfully";
 }
 ?>
